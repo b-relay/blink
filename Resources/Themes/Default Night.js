e179afb9813f88bdecfb2c17522c0ee60f37777f
Clay <257248983+clay-relay@users.noreply.github.com>
2026-02-11 03:03:50 -0500

    Darken Default Night accents for clearer contrast

--- Resources/Themes/Default Night.js
+++ Resources/Themes/Default Night.js
@@ -4,15 +4,15 @@ var green =      "#4E9A06";
 var yellow =     "#C4A000";
 var blue =       "#C4548C";
 var magenta =    "#75507B";
-var cyan =       "#D27A58";
+var cyan =       "#C86D49";
 var white =      "#D3D7CF";
 var lightBlack = "#555753";
 var lightRed =   "#EF2929";
 var lightGreen = "#00BA13";
 var lightYellow ="#FCE94F";
-var lightBlue =  "#F78FC7";
+var lightBlue =  "#E47BB4";
 var lightMagenta="#F200CB";
-var lightCyan =  "#F0B190";
+var lightCyan =  "#D99879";
 var lightWhite = "#EEEEEC";
 
 var paletteOverrides = {
@@ -22,14 +22,14 @@ var paletteOverrides = {
   12: lightBlue, 13: lightMagenta, 14: lightCyan, 15: lightWhite,
 
   // 256-color blues/cyans that often appear in CLIs (e.g. 39, 45, 75, 81).
-  17: "#7E5B99", 18: "#8A66A4", 19: "#9672B0", 20: "#A27DBB", 21: "#AE89C6",
-  24: "#8A675F", 25: "#96726A", 26: "#A27D76", 27: "#AE8981",
-  31: "#AA826F", 32: "#B78E7A", 33: "#C39A86",
-  38: "#CA9A84", 39: "#D6A68F",
-  44: "#D6A68F", 45: "#E3B29B", 51: "#F0BFA8",
-  69: "#D6A3C5", 75: "#E2B1D1", 81: "#EEC0DD", 87: "#FACFE9",
-  111: "#DEAFCF", 117: "#EABEDB", 123: "#F6CDE7",
-  159: "#F2D8EE", 195: "#FBE8F6"
+  17: "#6B4A82", 18: "#74528B", 19: "#7D5A95", 20: "#86639E", 21: "#8E6BA7",
+  24: "#76564F", 25: "#7F5E58", 26: "#876762", 27: "#906F6B",
+  31: "#946D5D", 32: "#9D7666", 33: "#A67E6F",
+  38: "#AC7F6B", 39: "#B48874",
+  44: "#B48874", 45: "#BD917D", 51: "#C69A86",
+  69: "#B584A8", 75: "#BF8FB3", 81: "#C89ABE", 87: "#D1A5C9",
+  111: "#BE8EAF", 117: "#C799BA", 123: "#D0A4C5",
+  159: "#CFABC8", 195: "#D8B5D2"
 };
 
 term_set('color-palette-overrides', paletteOverrides);
