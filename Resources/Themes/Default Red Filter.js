54a8ac07fa1493b0d7ac65dc5ffe8085e222628d
Clay <257248983+clay-relay@users.noreply.github.com>
2026-02-11 02:41:20 -0500

    Add default red-filter theme variant

---
+++ Resources/Themes/Default Red Filter.js
@@ -0,0 +1,25 @@
+var black =      "#000000";
+var red =        "#CC0000";
+var green =      "#4E9A06";
+var yellow =     "#C4A000";
+var blue =       "#C4548C";
+var magenta =    "#75507B";
+var cyan =       "#06989A";
+var white =      "#D3D7CF";
+var lightBlack = "#555753";
+var lightRed =   "#EF2929";
+var lightGreen = "#00BA13";
+var lightYellow ="#FCE94F";
+var lightBlue =  "#F78FC7";
+var lightMagenta="#F200CB";
+var lightCyan =  "#00B5BD";
+var lightWhite = "#EEEEEC";
+
+term_set('color-palette-overrides',
+         [ black      , red      , green     , yellow,
+           blue       , magenta  , cyan      , white,
+           lightBlack , lightRed , lightGreen, lightYellow,
+           lightBlue  , lightMagenta, lightCyan, lightWhite ]);
+
+term_set('cursor-color', 'rgba(63, 222, 233, 0.5)');
+term_set('cursor-blink', true);
