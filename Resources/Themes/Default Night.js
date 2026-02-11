916cd49bf683fbe3a491cae37ad327ffbfb8cdb3
Clay <257248983+clay-relay@users.noreply.github.com>
2026-02-11 03:11:01 -0500

    Increase Default Night contrast with darker blue 256 palette

--- Resources/Themes/Default Night.js
+++ Resources/Themes/Default Night.js
@@ -2,17 +2,17 @@ var black =      "#000000";
 var red =        "#CC0000";
 var green =      "#4E9A06";
 var yellow =     "#C4A000";
-var blue =       "#8059AD";
-var magenta =    "#6D4F8A";
-var cyan =       "#5F6E93";
+var blue =       "#4F3B83";
+var magenta =    "#593C89";
+var cyan =       "#3E4F78";
 var white =      "#D3D7CF";
 var lightBlack = "#555753";
 var lightRed =   "#EF2929";
 var lightGreen = "#00BA13";
 var lightYellow ="#FCE94F";
-var lightBlue =  "#9974BE";
-var lightMagenta="#9C6ABB";
-var lightCyan =  "#7686A9";
+var lightBlue =  "#6851A0";
+var lightMagenta="#7755A8";
+var lightCyan =  "#55668F";
 var lightWhite = "#EEEEEC";
 
 var paletteOverrides = {
@@ -22,14 +22,14 @@ var paletteOverrides = {
   12: lightBlue, 13: lightMagenta, 14: lightCyan, 15: lightWhite,
 
   // 256-color blues/cyans that often appear in CLIs (e.g. 39, 45, 75, 81).
-  17: "#4E4A84", 18: "#55508C", 19: "#5B5794", 20: "#625D9C", 21: "#6964A4",
-  24: "#4E5D78", 25: "#55657F", 26: "#5D6D87", 27: "#64758F",
-  31: "#5A6C86", 32: "#62748E", 33: "#697C96",
-  38: "#6A7F98", 39: "#7287A0",
-  44: "#7287A0", 45: "#7A8FA8", 51: "#8397B0",
-  69: "#705C9E", 75: "#7965A7", 81: "#836EB0", 87: "#8D77B9",
-  111: "#806AAA", 117: "#8973B3", 123: "#927CBC",
-  159: "#9A86C3", 195: "#A391CB"
+  17: "#35356B", 18: "#3A3A72", 19: "#3E3E79", 20: "#434380", 21: "#484887",
+  24: "#2F4A68", 25: "#35506F", 26: "#3B5676", 27: "#415D7D",
+  31: "#3E5875", 32: "#44607D", 33: "#4A6885",
+  38: "#4A6887", 39: "#50708F",
+  44: "#50708F", 45: "#567896", 51: "#5C809E",
+  69: "#4E4B89", 75: "#565391", 81: "#5E5B99", 87: "#6663A1",
+  111: "#5B5898", 117: "#6360A0", 123: "#6B68A8",
+  159: "#7370B0", 195: "#7B78B8"
 };
 
 term_set('color-palette-overrides', paletteOverrides);
